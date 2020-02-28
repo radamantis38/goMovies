e7ec0171a5b5e7d52704736d82738206ebeb94ee
Felipe Parra <fparra@merqueo.com>
2020-02-27 21:26:03 -0500

    v6

--- hello.go
+++ hello.go
@@ -3,7 +3,14 @@ package main
 import "errors"
 
 func Sum(x int, y int) (int, error) {
-	return 0, errors.New("Error")
+	return 0, nil
+}
+
+func AmenorB (a int , b int) error{
+	if a>=b {
+		return errors.New("no es menor")
+	}
+	return nil
 }
 
 func main() {
--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,21 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	total := Sum(5, 5)
-	if total != 10 {
-		t.Errorf("suma incorrecta, got: %d, want: %d.", total, 10)
+	main()
+	_, err:= Sum(5, 5)
+	if err != nil {
+		t.Errorf(err.Error())
 	}
+}
+
+func TestAmenorB(t *testing.T) {
+	err:= AmenorB(4, 5)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	err = AmenorB(6, 5)
+   if err == nil {
+	   t.Errorf("Fallo")
+   }
+
 }
